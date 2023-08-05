7efa3fb93547e475cf55613067a63696e81203a1
ADITYA LAD <97255480+Adityalad-25@users.noreply.github.com>
2023-08-05 19:29:19 +0530

    Update maximumSumSubarray.cpp

--- maximumSumSubarray.cpp
+++ maximumSumSubarray.cpp
@@ -1,13 +1,9 @@
-// Given an integer array nums, find the 
-// subarray
-//  with the largest sum, and return its sum.
-
-//  Example 1:
-
-// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-// Output: 6
-// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
-
+/* Given an integer array nums, find the subarray with the largest sum, and return its sum.
+ Example 1:
+ Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ Output: 6
+ Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+*/
 
 
 
